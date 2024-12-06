aacd0f70b38d798faa340ced19b2ee5bc50c56e5
Anton Keks <anton@azib.net>
2024-12-06 12:14:21 +0200

    add missing icons that Toasts use

--- ui/src/icons/Icon.svelte
+++ ui/src/icons/Icon.svelte
@@ -2,26 +2,14 @@
   import icons from './icons'
 
   export let name: string
-  export let size: 'xs' | 'sm' | '' | 'lg' = ''
-  export let strokeWidth: string = '2'
+  export let strokeWidth = '2'
 </script>
 
-{@html icons[name].replace('<svg', `<svg class="icon shrink-0 ${name} ${size} ${$$props.class ?? ''}" stroke-width=${strokeWidth} focusable="false"`)}
+{@html icons[name]?.replace('<svg', `<svg class="icon shrink-0 ${name} ${$$props.class ?? ''}" stroke-width=${strokeWidth} focusable="false"`) ?? 'NO-ICON-' + name}
 
-<style global>
-  .icon {
-    @apply w-5 h-5
-  }
-
-  .icon.xs {
-    @apply w-3 h-3
-  }
-
-  .icon.sm {
-    @apply w-4 h-4
-  }
-
-  .icon.lg {
-    @apply w-6 h-6
+<style>
+  :global(.icon) {
+    width: 1em;
+    height: 1em;
   }
 </style>
